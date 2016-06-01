d8e24916d9ab68050d100134d82fbc0dd05d2141
rafacasa <rafaelcasa.2007@yahoo.com.br>
2016-06-01 08:33:21 -0300

    Religador Eletromecanico com metodos prontos

--- src/SIGPROD2/Modelo/ReligadorEletromecanico.java
+++ src/SIGPROD2/Modelo/ReligadorEletromecanico.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+
 /**
  *
  * @author Rafael Casa
- * @version 19/05/16
+ * @version 01/06/2016
  */
 public class ReligadorEletromecanico implements Religador {
 
@@ -16,7 +17,7 @@ public class ReligadorEletromecanico implements Religador {
     private String modelo;
     private int codigo;
     private List<Double> fatorInicio;
-    private List<Double> tempoReligamento;
+    //private List<Double> tempoReligamento;
     private boolean[] existeCurva;
     private Map<Double, List<DialDeTempoMecanico>> mapaFaseRapidaPickupTempo;
     private Map<Double, List<DialDeTempoMecanico>> mapaNeutroRapidaPickupTempo;
@@ -27,9 +28,9 @@ public class ReligadorEletromecanico implements Religador {
     private List<Double> tempoDefinidaNeutraLenta;
     private List<Double> tempoDefinidaFaseRapida;
     private List<Double> tempoDefinidaNeutraRapida;
+
     /*private int qtdPontosFase;
     private int qtdPontosNeutro;*/
-
     public ReligadorEletromecanico() {
         this.fatorInicio = Arrays.asList(0.0, 0.0, 0.0, 0.0);
         this.correntePickup = Arrays.asList(null, null, null, null);
@@ -37,15 +38,132 @@ public class ReligadorEletromecanico implements Religador {
         this.mapaFaseRapidaPickupTempo = new TreeMap<>();
         this.mapaNeutroLentaPickupTempo = new TreeMap<>();
         this.mapaNeutroRapidaPickupTempo = new TreeMap<>();
-        this.tempoReligamento = new ArrayList<>();
+        //this.tempoReligamento = new ArrayList<>();
         this.tempoDefinidaFaseLenta = new ArrayList<>();
         this.tempoDefinidaFaseRapida = new ArrayList<>();
         this.tempoDefinidaNeutraRapida = new ArrayList<>();
         this.tempoDefinidaNeutraLenta = new ArrayList<>();
+        this.existeCurva = new boolean[4];
     }
-    
-    
-    
+
+    public void addCorrentePickup(ArrayList<Double> corrente, int tipo) {
+        this.correntePickup.set(tipo, new ArrayList<>(corrente));
+        if (tipo == INVERSA_FASE) {
+            for (Double c : corrente) {
+                this.mapaFaseLentaPickupTempo.put(c, new ArrayList<>());
+                this.mapaFaseRapidaPickupTempo.put(c, new ArrayList<>());
+            }
+        } else if (tipo == INVERSA_NEUTRO) {
+            for (Double c : corrente) {
+                this.mapaNeutroLentaPickupTempo.put(c, new ArrayList<>());
+                this.mapaNeutroRapidaPickupTempo.put(c, new ArrayList<>());
+            }
+        }
+    }
+
+    public void removeCorrentePickup(int tipo) {
+        this.correntePickup.set(tipo, new ArrayList<>());
+        if (tipo == INVERSA_FASE) {
+            this.mapaFaseRapidaPickupTempo = new TreeMap<>();
+            this.mapaFaseLentaPickupTempo = new TreeMap<>();
+        } else {
+            this.mapaNeutroRapidaPickupTempo = new TreeMap<>();
+            this.mapaNeutroLentaPickupTempo = new TreeMap<>();
+        }
+    }
+
+    public void addDialDeTempo(int tipo, boolean rapida, double corrente, double dial, ArrayList<PontoCurva> pontos) {
+        DialDeTempoMecanico dm = new DialDeTempoMecanico(dial, pontos);
+        List<DialDeTempoMecanico> array;
+
+        if (tipo == INVERSA_FASE) {
+            if (rapida) {
+                array = this.mapaFaseRapidaPickupTempo.get(corrente);
+                array.add(dm);
+                this.mapaFaseRapidaPickupTempo.put(corrente, array);
+                //this.qtdPontosFase += pontos.size();
+            } else {
+                array = this.mapaFaseLentaPickupTempo.get(corrente);
+                array.add(dm);
+                this.mapaFaseLentaPickupTempo.put(corrente, array);
+                //this.qtdPontosFase += pontos.size();
+            }
+
+        } else if (rapida) {
+            array = this.mapaNeutroRapidaPickupTempo.get(corrente);
+            array.add(dm);
+            this.mapaNeutroRapidaPickupTempo.put(corrente, array);
+            //this.qtdPontosNeutro += pontos.size();
+        } else {
+            array = this.mapaNeutroLentaPickupTempo.get(corrente);
+            array.add(dm);
+            this.mapaNeutroLentaPickupTempo.put(corrente, array);
+            //this.qtdPontosNeutro += pontos.size();
+        }
+    }
+
+    public void removeDialDeTempo(int tipo, boolean rapida) {
+        if (tipo == Rele.INVERSA_FASE) {
+            if (rapida) {
+                this.mapaFaseRapidaPickupTempo = new TreeMap<>();
+                //this.qtdPontosFase = 0;
+            } else {
+                this.mapaFaseLentaPickupTempo = new TreeMap<>();
+                //this.qtdPontosFase = 0;
+            }
+
+        } else if (rapida) {
+            this.mapaNeutroRapidaPickupTempo = new TreeMap<>();
+            //this.qtdPontosNeutro = 0;
+        } else {
+            this.mapaNeutroLentaPickupTempo = new TreeMap<>();
+            //this.qtdPontosFase = 0;
+        }
+    }
+
+    public List<Double> getDialDeTempo(int tipo, boolean rapida, double corrente) {
+        List<Double> dial = new ArrayList<>();
+        List<DialDeTempoMecanico> diais;
+        if (tipo == INVERSA_FASE) {
+            if (rapida) {
+                diais = this.mapaFaseRapidaPickupTempo.get(corrente);
+            } else {
+                diais = this.mapaFaseLentaPickupTempo.get(corrente);
+            }
+        } else if (rapida) {
+            diais = this.mapaNeutroRapidaPickupTempo.get(corrente);
+        } else {
+            diais = this.mapaNeutroLentaPickupTempo.get(corrente);
+        }
+
+        for (DialDeTempoMecanico d : diais) {
+            dial.add(d.getDial());
+        }
+        return dial;
+    }
+
+    public List<PontoCurva> getPontosDialDeTempo(double corrente, double dial, int tipo, boolean rapida) {
+        List<DialDeTempoMecanico> dt;
+        if (tipo == Rele.INVERSA_FASE) {
+            if (rapida) {
+                dt = this.mapaFaseRapidaPickupTempo.get(corrente);
+            } else {
+                dt = this.mapaFaseLentaPickupTempo.get(corrente);
+            }
+        } else if (rapida) {
+            dt = this.mapaNeutroRapidaPickupTempo.get(corrente);
+        } else {
+            dt = this.mapaNeutroLentaPickupTempo.get(corrente);
+        }
+
+        for (DialDeTempoMecanico d : dt) {
+            if (d.getDial() == dial) {
+                return d.getPontosCurva();
+            }
+        }
+        return null;
+    }
+
     @Override
     public double getFatorInicio(int tipo) {
         return this.fatorInicio.get(tipo);
